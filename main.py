104565a4c093b30aaaec792ea8d840168fccdf07
George Sparrow <sparrowg@iusb.edu>
2016-05-27 01:08:56 -0400

    Adds Paras

--- main.py
+++ main.py
@@ -75,11 +75,11 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Jigglypuff (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Wigglytuff (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Zubat      (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
-my_pokemon  = pokemon.Golbat     (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
+#my_pokemon  = pokemon.Golbat     (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
 #my_pokemon  = pokemon.Oddish     (  0, 0, 0, 0, 1, NEAR_BLACK, (74,165,90),   (165,214,132), WHITE, screen)
 #my_pokemon  = pokemon.Gloom      (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
 #my_pokemon  = pokemon.Vileplume  (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
-# TODO  46 - Paras
+my_pokemon  = pokemon.Paras      (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
 # TODO  47 - Parasect
 #my_pokemon  = pokemon.Venonat    (  0, 0, 0, 0, 1, NEAR_BLACK, (173,123,189), (222,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Venomoth   (  0, 0, 0, 0, 1, NEAR_BLACK, (173,123,189), (222,181,197), WHITE, screen)
