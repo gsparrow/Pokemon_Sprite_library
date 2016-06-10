843c79b3d393667d7084a7eaf471274e3a422855
George Sparrow <sparrowg@iusb.edu>
2016-06-09 21:28:28 -0600

    Adds Farfetch'd

--- main.py
+++ main.py
@@ -115,10 +115,10 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Slowpoke   (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Slowbro    (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197),  WHITE, screen)
 #my_pokemon  = pokemon.Magnemite  (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
-my_pokemon  = pokemon.Magneton   (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
-# TODO  83 - Farfetch'd
-#my_pokemon  = pokemon.Doduo     (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74), (230,165,123), WHITE, screen)
-#my_pokemon  = pokemon.Dodrio    (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74), (230,165,123), WHITE, screen)
+#my_pokemon  = pokemon.Magneton   (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
+my_pokemon  = pokemon.Farfetchd  (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74), (230,165,123), WHITE, screen)
+#my_pokemon  = pokemon.Doduo      (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74), (230,165,123), WHITE, screen)
+#my_pokemon  = pokemon.Dodrio     (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74), (230,165,123), WHITE, screen)
 # TODO  86 - Seel
 # TODO  87 - Dewgong
 # TODO  88 - Grimer
