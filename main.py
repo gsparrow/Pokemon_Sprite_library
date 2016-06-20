1d082a77f06246bb06b97516440f8c4d8a06349a
George Sparrow <sparrowg@iusb.edu>
2016-06-19 21:13:58 -0600

    Adds Hitmonlee

--- main.py
+++ main.py
@@ -136,10 +136,10 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Voltorb    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,165,0), (255,230,115), WHITE, screen)
 #my_pokemon  = pokemon.Electrode  (  0, 0, 0, 0, 1, NEAR_BLACK, (214,165,0), (255,230,115), WHITE, screen)
 #my_pokemon  = pokemon.Exeggcute  (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
-my_pokemon  = pokemon.Exeggutor  (  0, 0, 0, 0, 1, NEAR_BLACK, (74,165,90),   (165,214,132), WHITE, screen)
+#my_pokemon  = pokemon.Exeggutor  (  0, 0, 0, 0, 1, NEAR_BLACK, (74,165,90),   (165,214,132), WHITE, screen)
 #my_pokemon  = pokemon.Cubone     (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
 #my_pokemon  = pokemon.Marowak    (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
-# TODO 106 - Hitmonlee
+my_pokemon  = pokemon.Hitmonlee  (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74),  (230,165,123), WHITE, screen)
 # TODO 107 - Hitmonchan
 # TODO 108 - Lickitung
 #my_pokemon  = pokemon.Koffing    (  0, 0, 0, 0, 1, NEAR_BLACK, (173,123,189), (222,181,197), WHITE, screen)
