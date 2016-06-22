fe68a84d97c635ed83e7107f32c3931c09735c6b
George Sparrow <sparrowg@iusb.edu>
2016-06-22 05:38:04 -0600

    Adds Rhyhorn

--- main.py
+++ main.py
@@ -141,10 +141,10 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Marowak    (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
 #my_pokemon  = pokemon.Hitmonlee  (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74),  (230,165,123), WHITE, screen)
 #my_pokemon  = pokemon.Hitmonchan (  0, 0, 0, 0, 1, NEAR_BLACK, (173,115,74),  (230,165,123), WHITE, screen)
-my_pokemon  = pokemon.Lickitung  (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
+#my_pokemon  = pokemon.Lickitung  (  0, 0, 0, 0, 1, NEAR_BLACK, (230,123,173), (247,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Koffing    (  0, 0, 0, 0, 1, NEAR_BLACK, (173,123,189), (222,181,197), WHITE, screen)
 #my_pokemon  = pokemon.Weezing    (  0, 0, 0, 0, 1, NEAR_BLACK, (173,123,189), (222,181,197), WHITE, screen)
-# TODO 111 - Rhyhorn
+my_pokemon  = pokemon.Rhyhorn    (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
 # TODO 112 - Rhydon
 # TODO 113 - Chansey
 #my_pokemon  = pokemon.Tangela    (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189), (148,165,222), WHITE, screen)
