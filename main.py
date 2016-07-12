cc64506ddf9122369f746e90a3593fe681904178
George Sparrow <sparrowg@iusb.edu>
2016-07-11 22:02:18 -0600

    Adds Scyther

--- main.py
+++ main.py
@@ -152,11 +152,11 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Horsea     (  0, 0, 0, 0, 1, NEAR_BLACK, (115,156,206), (173,206,239), WHITE, screen)
 #my_pokemon  = pokemon.Seadra     (  0, 0, 0, 0, 1, NEAR_BLACK, (115,156,206), (173,206,239), WHITE, screen)
 #my_pokemon  = pokemon.Goldeen    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
-my_pokemon  = pokemon.Seaking    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
+#my_pokemon  = pokemon.Seaking    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,165,82),  WHITE, screen)
 #my_pokemon  = pokemon.Staryu     (  0, 0, 0, 0, 1, NEAR_BLACK, (255,165,82),  (214,82,49),   WHITE, screen)
 #my_pokemon  = pokemon.Starmie    (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
 #my_pokemon  = pokemon.Mr_Mime    (  0, 0, 0, 0, 1, NEAR_BLACK, (247,181,197), (230,123,173), WHITE, screen)
-# TODO 123 - Scyther
+my_pokemon  = pokemon.Scyther    (  0, 0, 0, 0, 1, NEAR_BLACK, (74,165,90),   (165,214,132), WHITE, screen)
 # TODO 124 - Jynx
 # TODO 125 - Electabuzz
 # TODO 126 - Magmar
