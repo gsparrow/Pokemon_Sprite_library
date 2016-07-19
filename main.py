1374ce911e074e22ce70e76b01f1c9e7d70fa09b
George Sparrow <sparrowg@iusb.edu>
2016-07-18 20:16:28 -0600

    Adds Omastar

--- main.py
+++ main.py
@@ -171,8 +171,8 @@ clock = pygame.time.Clock()
 #my_pokemon  = pokemon.Jolteon    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,165,0),   (255,230,115), WHITE, screen)
 #my_pokemon  = pokemon.Flareon    (  0, 0, 0, 0, 1, NEAR_BLACK, (214,82,49),   (255,162,82),  WHITE, screen)
 #my_pokemon  = pokemon.Porygon    (  0, 0, 0, 0, 1, NEAR_BLACK, (123,123,148), (214,173,181), WHITE, screen)
-my_pokemon  = pokemon.Omanyte    (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
-# TODO 139 - Omastar
+#my_pokemon  = pokemon.Omanyte    (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
+my_pokemon  = pokemon.Omastar    (  0, 0, 0, 0, 1, NEAR_BLACK, (90,123,189),  (148,165,222), WHITE, screen)
 # TODO 140 - Kabuto
 # TODO 141 - Kabutops
 # TODO 142 - Aerodactyl
